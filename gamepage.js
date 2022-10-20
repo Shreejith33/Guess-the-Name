db7b94657d32804d591c7b29aa8081616ef720da
Shreejith33 <93464358+Shreejith33@users.noreply.github.com>
2022-10-19 19:46:17 -0700

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,27 @@
+  var y = Math.floor(Math.random() * 10 + 1);
+  var guess = 1
+
+  function submit()
+  {
+    var x = document.getElementById("guessField").value;
+    if(x==y)
+    {
+        alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT! (In " +guess+ " GUESS)");
+        guess = 1;
+    }
+    else if(x > y)
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A SMALLER NUMBER!");
+    }
+    else
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A GREATER NUMBER!");
+    }
+  }
+  
+  function playAgain()
+  {
+    y = Math.floor(Math.random() * 10 + 1);
+  }
